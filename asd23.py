b35850b453632352b5a5602d9e307b74c3e38930
sorosle <soros.levente.tech2021d@bolyaimovar.com>
2022-10-27 10:35:37 +0200

    cuccoksz

--- asd23.py
+++ asd23.py
@@ -1,6 +1,6 @@
 homerseklet = float(input("Add meg az alap hőfokot?"))
 mertekegyseg = input("Add meg a mértékegységet C=celsius F=Farenheit K=Kelvin")
-valto = input("Add meg miri szeretnéd átváltami C=celsius F=farenheit K=kelvin")
+valto = input("Add meg mire szeretnéd átváltami C=celsius F=farenheit K=kelvin")
 
 if mertekegyseg == "C" and valto == "F":
     print("a Hőmérséklet farenheitben: ", homerseklet * 1.8 + 32)
@@ -20,6 +20,3 @@ elif mertekegyseg == "K" and valto == "F":
     print("a Hőmérséklet farenheitben: ", (homerseklet - 273.15) * 1.8 + 32)
 elif mertekegyseg == "K" and valto == "K":
     print("a Hőmérséklet Kelvinben: ",homerseklet)
-
-
-
---
+++ while gyak.py
@@ -0,0 +1,58 @@
+a = 10
+b = 0
+
+while b == 0:
+    b = float(input())
+
+
+print("Azt hiszed majd én számolok neked kicsiny ?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
