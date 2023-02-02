d9c31e57db07c9a35e5673a4dd2723241e8af5e7
sorosle <soros.levente.tech2021d@bolyaimovar.com>
2023-02-02 10:23:27 +0100

    cuccoksz

---
+++ keres kutat talál.py
@@ -0,0 +1,27 @@
+def kicsiny(lista):
+    legkisebb = lista[0]
+    for i in lista:
+        if i < legkisebb:
+            legkisebb = i
+
+    return legkisebb
+
+
+def kilep(szam):
+    if szam == 0:
+        return True
+    else:
+        return False
+
+
+lista = []
+
+while True:
+    a = int(input("adj meg számot"))
+    if kilep(a):
+        break
+    else:
+        lista.append(a)
+
+kis = kicsiny(lista)
+print(kis)
